531ac011f6f86f9a6e7b216c91a09b6796e141c9
mcollymoreabbas <miacollymoreabbas@gmail.com>
2023-06-16 18:43:29 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -92,7 +92,7 @@ function loadData() {
 
       allData.push(peopleData);
       allData.push(locationGeoJSON);
-      $.getJSON("data/filteredNYBoundaries.json").then(function (boundaries) {
+      $.getJSON("data/NYBoundaries.json").then(function (boundaries) {
         allData.push(boundaries);
       });
       // leafletMap = new LeafletMap("leaflet-map", allData, [43.2994, -74.21798]);
