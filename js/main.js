6922d84fd0f4c3b84906c019d88bdcdb9b4f6d52
miacollymoreabbas <mcollymoreabbas@wustl.edu>
2023-06-16 18:24:59 -0400

    for github pages

---
+++ js/main.js
@@ -0,0 +1,119 @@
+var allData = [];
+
+// Variable for the visualization instance
+var leafletMap, mapboxMap;
+// Start application by loading the data
+loadData();
+
+function loadData() {
+  // Hubway XML station feed
+
+  var locationUrl = "data/KastorLocationsNY.json";
+  var peopleUrl = "data/KastorPeopleNY.json";
+  var locationData = [];
+  var peopleData = [];
+  var locationGeoJSON = {};
+  var temp = [];
+  var peopleGeoJSON = [];
+  var peopleFields = [];
+  // TO-DO: LOAD DATA
+
+  //use data
+  //  console.log(data)
+
+  $.getJSON(locationUrl)
+    .then(function (lData) {
+      // ...worked, put it in #view-graphic
+      lData = lData.Data;
+      lData.sort(function (a, b) {
+        return a["First Year"] - b["First Year"];
+      });
+      // get rid of duplicates at some poitn
+
+      lData.forEach(function (d) {
+        temp.push(
+          JSON.parse(
+            '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [' +
+              d.x +
+              "," +
+              d.y +
+              ']}, "properties": { "name": "' +
+              d["Locality"] +
+              '", ' +
+              '"firstYear": ' +
+              d["First Year"] +
+              ', "geoID": "' +
+              d["Geocoding ID"] +
+              '",' +
+              '"personnel":[]' + ',"buildingType": "' + d["Building"]+ '"' +
+              "}}"
+          )
+        );
+      });
+
+      locationGeoJSON = { type: "FeatureCollection", features: temp };
+
+      // console.log(locationGeoJSON);
+      allData.push(lData);
+    })
+    .fail(function () {
+      // ...didn't work, handle it
+    });
+  $.getJSON(peopleUrl)
+    .then(function (pData) {
+      pData.Fields.forEach((element) => {
+        peopleFields.push(element["Fied"]);
+      });
+      peopleFields.push("Geocoding ID");
+
+      pData.Data.map((item) => {
+        // console.log(item)
+        const filteredItem = {};
+        peopleFields.forEach((field) => {
+          if (item.hasOwnProperty(field)) {
+            filteredItem[field] = item[field];
+          }
+        });
+        peopleData.push(filteredItem);
+      });
+
+      peopleData.sort(function (a, b) {
+        return a.GovernmentEmployeeNumber - b.GovernmentEmployeeNumber;
+      });
+      // issue with the personnel popping up right now
+
+      peopleData.forEach(function (p) {
+        locationGeoJSON["features"].forEach(function (t) {
+          if (p["Geocoding ID"] == t.properties.geoID) {
+            t.properties["personnel"].push(p);
+          }
+        });
+      });
+
+      allData.push(peopleData);
+      allData.push(locationGeoJSON);
+      $.getJSON("data/filteredNYBoundaries.json").then(function (boundaries) {
+        allData.push(boundaries);
+      });
+      // leafletMap = new LeafletMap("leaflet-map", allData, [43.2994, -74.21798]);
+      mapboxMap = new MapBoxMap("mapbox-map", allData, [-75.5, 43]);
+      mapboxMap.initVis();
+
+      document
+        .getElementById("timeSlider")
+        .addEventListener("change", function (e) {
+          document.getElementById("yearLabel").innerText =
+            "Current Year: " + e.target.value;
+          // ok so when e changes, get the value from the slider and then update mapBox
+          // mapboxMap.removeMarkers();
+          mapboxMap.addMarkers(e.target.value);
+          // mapboxMap.plotMarkers(e.target.value);
+          // leafletMap.changeYear(e.target.value);
+        });
+    })
+    .fail(function () {
+      // ...didn't work, handle it
+    });
+}
+
+function createVis() {}
---
+++ js/mapbox.js
@@ -0,0 +1,200 @@
+MapBoxMap = function (_parentElement, _data, _mapPosition) {
+  this.parentElement = _parentElement;
+  this.data = _data;
+  this.mapPosition = _mapPosition;
+  this.currMarkers = [];
+  this.currYear = 1789;
+  this.markerArr = [];
+
+  // data[0] is locationsdata
+  // eventually should get rid of this
+  // data[1] is people
+  // data[2] is locationsgeojson
+  // data [3] is countiesgeojson
+};
+
+MapBoxMap.prototype.initVis = function () {
+  var vis = this;
+
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoia2FzdG9ycHJvamVjdCIsImEiOiJjbGlkZ2Q0Z3Ywc2N5M2RwZjVrcnJhMmNvIn0.e1tZnLbd-wfMVODWHJH4ew";
+  const map = new mapboxgl.Map({
+    container: vis.parentElement, // container ID
+    style: "mapbox://styles/mapbox/satellite-v9", // style URL
+    center: vis.mapPosition, // starting position [lng, lat]
+    zoom: 6, // starting zoom
+  });
+
+  // initial load of the map
+  map.on("load", () => {
+    // county data
+    map.addSource("countyBoundaries", {
+      type: "geojson",
+      data: vis.data[3],
+      generateId: true,
+    });
+    console.log(vis.data);
+    map.addLayer({
+      id: "counties",
+      source: "countyBoundaries",
+      type: "fill",
+      paint: {
+        "fill-color": "#CCFF66",
+        "fill-opacity": 0.3,
+        "fill-outline-color": "black",
+      },
+    });
+    // building data
+    map.addSource("buildings", {
+      type: "geojson",
+      data: vis.data[2],
+      generateId: true,
+      cluster: true,
+      clusterMaxZoom: 7,
+      clusterRadius: 40,
+    });
+
+    map.addLayer({
+      id: "buildings-viz",
+      type: "circle",
+      source: "buildings",
+      paint: {
+        "circle-radius": ["step", ["get", "point_count"], 10, 20, 30],
+        "circle-color": "#301934",
+      },
+    });
+    vis.map.on("mousemove", "buildings-viz", function (e) {
+      // Get the marker's description from the dataset
+      var currData = e.features[0].properties;
+      document.getElementById("Name").innerHTML = currData.name;
+      document.getElementById("State").innerHTML = "New York";
+      document.getElementById("BuildingYear").innerHTML = currData.firstYear;
+      document.getElementById("Coords").innerHTML =
+        "(" +
+        Math.round(10 * e.lngLat["lng"]) / 10 +
+        "," +
+        Math.round(10 * e.lngLat["lat"]) / 10 +
+        ")";
+
+      console.log(currData);
+      document.getElementById("Personnel").innerHTML += "<ul> </ul>";
+      // currData.personnel.forEach(function (d) {
+      //   var personnelList = document.getElementById("personnel");
+      //   var li = document.createElement("li");
+      //   li.appendChild(document.createTextNode(d));
+      //   personnelList.appendChild(li);
+      // });
+
+      // Show a tooltip with the description
+    });
+    // remove tooltip when off building
+    vis.map.on("mouseleave", "buildings-viz", function (e) {
+      document.getElementById("Name").innerHTML = "";
+      document.getElementById("State").innerHTML = "";
+      document.getElementById("BuildingYear").innerHTML = "";
+      document.getElementById("Coords").innerHTML = "";
+      document.getElementById("Personnel").innerHTML = "";
+    });
+  });
+
+  vis.map = map;
+
+  console.log(vis.data);
+
+  // a lot of mapbox can be entered and edited dynamically without further functions,
+  // which is quicker for the loading of the data
+};
+
+MapBoxMap.prototype.updateVis = function () {
+  // update vis with the time slider
+};
+
+MapBoxMap.prototype.changeYear = function (newYear) {
+  var vis = this;
+
+  var markersToMove = [];
+
+  if (newYear > vis.currYear) {
+    markersToMove = vis.data[0].filter(function (d) {
+      return d["First Year"] <= newYear && d["First Year"] >= vis.currYear;
+    });
+    vis.addMarkers(markersToMove);
+  } else {
+  }
+  vis.currMarkers = [];
+  vis.currMarkers = vis.data[0].filter(function (d) {
+    return d["First Year"] <= newYear;
+  });
+  vis.addMarkers(vis.currMarkers);
+
+  vis.currYear = newYear;
+  // vis.removeMarkers();
+};
+
+// coujld eventually make all the add and remove markers stuff private?
+MapBoxMap.prototype.addMarkers = function (newMarkers) {
+  var vis = this;
+
+  vis.removeMarkers();
+  // console.log(newMarkers);
+  // location is name
+  // newMarkers.forEach(function (m) {
+  //   var el = document.createElement("div");
+  //   el.className = m["Geocoding ID"];
+  //   el.data = m;
+
+  //   var currMarker = new mapboxgl.Marker().setLngLat([m.x, m.y]).addTo(vis.map);
+  //   vis.markerArr.push(currMarker);
+  //   // console.log(currMarker);
+  // });
+
+  // Add hover effect to markers
+  vis.map.on("mousemove", "buildings-viz", function (e) {
+    // Get the marker's description from the dataset
+    var currData = e.features[0].properties;
+    document.getElementById("Name").innerHTML = currData.name;
+    document.getElementById("State").innerHTML = "New York";
+    document.getElementById("BuildingYear").innerHTML = currData.firstYear;
+    document.getElementById("Coords").innerHTML =
+      "(" +
+      Math.round(10 * e.lngLat["lng"]) / 10 +
+      "," +
+      Math.round(10 * e.lngLat["lat"]) / 10 +
+      ")";
+
+    console.log(currData);
+    document.getElementById("Personnel").innerHTML += "<ul> </ul>";
+    // currData.personnel.forEach(function (d) {
+    //   var personnelList = document.getElementById("personnel");
+    //   var li = document.createElement("li");
+    //   li.appendChild(document.createTextNode(d));
+    //   personnelList.appendChild(li);
+    // });
+
+    // Show a tooltip with the description
+  });
+  // remove tooltip when off building
+  vis.map.on("mouseleave", "buildings-viz", function (e) {
+    document.getElementById("Name").innerHTML = "";
+    document.getElementById("State").innerHTML = "";
+    document.getElementById("BuildingYear").innerHTML = "";
+    document.getElementById("Coords").innerHTML = "";
+    document.getElementById("Personnel").innerHTML = "";
+  });
+};
+
+// future implementation of add and remove to help with large data files
+
+MapBoxMap.prototype.removeMarkers = function (oldMarkers) {
+  var vis = this;
+
+  // if(vis.map.getLayer('buildings')){
+  //   vis.map.removeLayer('buildings');
+  // }
+  vis.markerArr.forEach(function (marker) {
+    // Apply selection style or perform other actions
+    marker.remove();
+  });
+  vis.currMarkers = [];
+  vis.markerArr = [];
+};
