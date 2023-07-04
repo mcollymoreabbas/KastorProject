f1a8ae3474fc580905ae0ecd97ee5727b867319c
miacollymoreabbas <mcollymoreabbas@wustl.edu>
2023-07-04 14:45:35 -0400

    Map Toggle

--- js/main.js
+++ js/main.js
@@ -8,13 +8,15 @@ loadData();
 function loadData() {
   // Hubway XML station feed
 
-  var locationUrl = "./data/KastorLocationsNY.json";
+  var locationUrl = "./data/AllLocations.json";
   var peopleUrl = "./data/KastorPeopleNY.json";
-  var locationData = [];
+  var taxUrl = "./data/1798_Tax_Divisions_Merged.json";
+  var districtUrl = "./data/1814_Districts_Merged.json";
+  var countyUrl = "./data/CountyBoundaries.json";
+  var stateUrl = "./data/StateBoundaries.json";
   var peopleData = [];
   var locationGeoJSON = {};
   var temp = [];
-  var peopleGeoJSON = [];
   var peopleFields = [];
   // TO-DO: LOAD DATA
 
@@ -22,47 +24,26 @@ function loadData() {
   $.when(
     $.getJSON(locationUrl),
     $.getJSON(peopleUrl),
-    $.getJSON("./data/CountyBoundaries.json"),
-    $.getJSON("./data/StateBoundaries.json")
+    $.getJSON(countyUrl),
+    $.getJSON(stateUrl),
+    $.getJSON(taxUrl),
+    $.getJSON(districtUrl)
   )
-    .then(function (lData, pData, countyBoundaries, stateBoundaries) {
+    .then(function (
+      lData,
+      pData,
+      countyBoundaries,
+      stateBoundaries,
+      taxBoundaries,
+      districtBoundaries
+    ) {
       lData = lData[0];
       pData = pData[0];
       countyBoundaries = countyBoundaries[0];
       stateBoundaries = stateBoundaries[0];
-
-      // ...worked, put it in #view-graphic
+      taxBoundaries = taxBoundaries[0];
+      districtBoundaries = districtBoundaries[0];
       lData = lData["Data"];
-      lData.sort(function (a, b) {
-        return a["First Year"] - b["First Year"];
-      });
-      // get rid of duplicates at some poitn
-
-      lData.forEach(function (d) {
-        // this is the issue, you are using the + which is fro a string ig
-
-        temp.push(
-          JSON.parse(
-            '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [' +
-              d.x +
-              "," +
-              d.y +
-              ']}, "properties": { "name": "' +
-              d["Locality"] +
-              '", ' +
-              '"firstYear": ' +
-              d["First Year"] +
-              ', "geoID": "' +
-              d["Geocoding ID"] +
-              '", ' +
-              '"buildingType": "' +
-              d["Building"] +
-              '"' +
-              "}}"
-          )
-        );
-      });
-      locationGeoJSON = { type: "FeatureCollection", features: temp };
 
       pData["Fields"].forEach((element) => {
         peopleFields.push(element["Fied"]);
@@ -82,20 +63,74 @@ function loadData() {
       peopleData.sort(function (a, b) {
         return a.GovernmentEmployeeNumber - b.GovernmentEmployeeNumber;
       });
-      // issue with the personnel popping up right now
 
-      locationGeoJSON["features"].forEach(function (t) {
-        t.properties["personnel"] = [];
-        peopleData.forEach(function (p) {
-          if (p["Geocoding ID"] == t.properties.geoID) {
-            t.properties["personnel"].push(p);
-          }
+      lData.forEach(function (d) {
+        // this is the issue, you are using the + which is fro a string ig
+        const coordinates = [parseFloat(d.x), parseFloat(d.y)];
+
+        if (isNaN(coordinates[0]) || isNaN(coordinates[1])) {
+          // Handle the case where d.x or d.y is not a valid number
+          // console.error("Invalid coordinates:", d.x, d.y);
+        } else {
+          const jsonString = JSON.stringify({
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [d.y, d.x],
+            },
+            properties: {
+              name: d["Geocoding Location"],
+              country: d["Country"],
+              polity: d["Polity"],
+              sector: d["Sector"],
+              geoID: d["Geocode Number"],
+              state: d["State"],
+              buildingType: d["Type"],
+            },
+          });
+
+          // Parse the JSON string
+          const parsedData = JSON.parse(jsonString);
+          temp.push(parsedData);
+        }
+      });
+
+      locationGeoJSON = { type: "FeatureCollection", features: temp };
+      // issue with the personnel popping up right now
+      // filter and merge all the personnel to one object right now
+      locationGeoJSON.features.forEach(function (loc) {
+        var temp = peopleData.filter(function (p) {
+          return p["Geocoding ID"] == loc.properties.geoID;
         });
+        if (temp.length > 0) {
+          var personnelArr = [
+            {
+              employeeID: temp[0]["GovernmentEmployeeNumber"],
+              appointments: temp,
+            },
+          ];
+          var smallestYear = temp.reduce((smallest, obj) => {
+            return obj["Record Year"] < smallest
+              ? obj["Record Year"]
+              : smallest;
+          }, Infinity);
+          var biggestYear = temp.reduce((biggest, obj) => {
+            return obj["Record Year"] >= biggest ? obj["Record Year"] : biggest;
+          }, -Infinity);
+          loc.properties.firstYear = smallestYear;
+          loc.properties.lastYear = biggestYear;
+          loc.properties.personnel = personnelArr;
+          // if(loc.properties.state == "NY"){
+          //   console.log(loc)
+          // }
+        }
       });
 
       allData.push(locationGeoJSON);
       allData.push(countyBoundaries);
       allData.push(stateBoundaries);
+      allData.push(taxBoundaries);
+      allData.push(districtBoundaries);
       mapboxMap = new MapBoxMap("mapbox-map", allData);
       mapboxMap.initVis();
     })
--- js/mapbox.js
+++ js/mapbox.js
@@ -5,6 +5,8 @@ MapBoxMap = function (_parentElement, _data) {
   // data[0] is locationsgeoJSON
   // data [1] is countyBoundariesgeoJSON
   //data [2] is stateBoundariesgeoJSON
+  //data[3] is taxes
+  //data[4] is 1814 district
 };
 
 MapBoxMap.prototype.initVis = function () {
@@ -21,8 +23,6 @@ MapBoxMap.prototype.initVis = function () {
     zoom: 6, // starting zoom
   });
 
-  // non-prototyupe function for loading personnel?
-
   // initial load of the map
   map.on("load", () => {
     // county data
@@ -37,12 +37,6 @@ MapBoxMap.prototype.initVis = function () {
         return bool;
       }), // Filter the array based on a condition
     };
-    var originalLocData = {
-      ...vis.data[0],
-      features: vis.data[0].features.filter(function (d) {
-        return d.properties.firstYear <= 1832;
-      }),
-    };
 
     var originalStateData = {
       ...vis.data[2], // Spread the existing object properties
@@ -55,69 +49,166 @@ MapBoxMap.prototype.initVis = function () {
         return bool;
       }), // Filter the array based on a condition
     };
+
     // BOUNDARY SOURCE DATA
     map.addSource("countyBoundaries", {
       type: "geojson",
       data: originalCountyData,
       generateId: true,
     });
-
     map.addSource("stateBoundaries", {
       type: "geojson",
       data: originalStateData,
       generateId: true,
     });
-
     // LOCATION SOURCE DATA
     map.addSource("locations", {
       type: "geojson",
-      data: originalLocData,
+      data: vis.data[0],
       generateId: true,
       cluster: true,
       clusterMaxZoom: 7,
       clusterRadius: 40,
     });
+    //TAX SOURCE DATA
+    map.addSource("taxes", {
+      type: "geojson",
+      data: vis.data[3],
+      generateId: true,
+    });
+    //1814 DISTRICT DATA
+    map.addSource("district", {
+      type: "geojson",
+      data: vis.data[4],
+      generateId: true,
+    });
 
-    // COUNTY LAYER
-
+    // COUNTY OUTLINE LAYER
     map.addLayer({
-      id: "counties",
+      id: "County Boundaries",
+      source: "countyBoundaries",
+      type: "line",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+        visibility: "none",
+      },
+      paint: {
+        "line-color": "white",
+        "line-width": 2,
+      },
+    });
+
+    // COUNTY FILL LAYER
+    map.addLayer({
+      id: "County Fill",
       source: "countyBoundaries",
       type: "fill",
+      layout: { visibility: "none" },
       paint: {
         "fill-color": "#EE4B2B",
-        "fill-opacity": 0.15,
+        "fill-opacity": 0,
+        "fill-outline-color": "green",
+      },
+    });
+    // STATE OUTLINE LAYER
+    map.addLayer({
+      id: "State Boundaries",
+      source: "stateBoundaries",
+      type: "line",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+        visibility: "none",
+      },
+      // interpolation numbers need work
+      paint: {
+        "line-color": [
+          "interpolate",
+          ["linear"],
+          ["get", "ID_NUM"],
+          0,
+          "#FF0000",
+          20,
+          "#FFFF00",
+          50,
+          "#A020F0",
+          80,
+          "#00FF00",
+          110,
+          "#FFA500",
+          150,
+          "#FFC0CB",
+          180,
+          "#808080",
+        ],
+        "line-width": 6,
+      },
+    });
+    // STATE FILL LAYER
+    map.addLayer({
+      id: "State Fill",
+      source: "stateBoundaries",
+      type: "fill",
+      layout: { visibility: "none" },
+      paint: {
+        "fill-color": "#EE4B2B",
+        "fill-opacity": 0,
         "fill-outline-color": "white",
       },
     });
-
+    // TAX OUTLINE LAYER
     map.addLayer({
-      id: "countyOutline",
-      source: "countyBoundaries",
+      id: "Tax Outlines",
+      source: "taxes",
       type: "line",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+        visibility: "none",
+      },
       paint: {
+        "line-color": "blue",
         "line-width": 2,
-        "line-color": "white",
       },
     });
-
-    // map.addLayer({
-    //   id: "states",
-    //   source: "stateBoundaries",
-    //   type: "fill",
-    //   paint: {
-    //     "fill-color": "#EE4B2B",
-    //     "fill-opacity": 0.3,
-    //     "fill-outline-color": "black",
-    //   },
-    // });
-
+    // TAX FILL LAYER
     map.addLayer({
-      id: "stateOutline",
-      source: "stateBoundaries",
-      type: "line",
+      id: "1798 Tax Divisions",
+      source: "taxes",
+      type: "fill",
+      layout: { visibility: "none" },
       paint: {
-        "line-width": 4,
+        "fill-color": "#EE4B2B",
+        "fill-opacity": 0,
+        "fill-outline-color": "white",
+      },
+    });
+    // DISTRICT OUTLINE LAYER
+    map.addLayer({
+      id: "District Outlines",
+      source: "district",
+      type: "line",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+        visibility: "none",
+      },
+      paint: {
+        "line-color": "red",
+        "line-width": 2,
+      },
+    });
+    // DISTRICT FILL LAYER
+    map.addLayer({
+      id: "1814 District",
+      source: "district",
+      type: "fill",
+      layout: { visibility: "none" },
+      paint: {
+        "fill-color": "#EE4B2B",
+        "fill-opacity": 0,
+        "fill-outline-color": "white",
       },
     });
 
@@ -131,7 +222,6 @@ MapBoxMap.prototype.initVis = function () {
         "circle-color": [
           "step",
           ["get", "point_count"],
-
           "#b8e2f6",
           4,
           "#7bbad8",
@@ -178,14 +268,9 @@ MapBoxMap.prototype.initVis = function () {
       },
     });
 
-    // map.on("mouseenter", "clusters", function (e, item) {
-    //   console.log(e, item);
-    // });
     var locID = null;
 
     map.on("click", "clusters", (e) => {
-      // TODO: FIX BUG WITH THIS CODE
-      // maybe this one can just create a popup that'll have a UL
       const features = map.queryRenderedFeatures(e.point, {
         layers: ["clusters"],
       });
@@ -211,23 +296,18 @@ MapBoxMap.prototype.initVis = function () {
                 "</strong> <br> ";
             });
           }
-          document.getElementById("displayList").innerHTML = clusterString;
+          document.getElementById("clusterList").innerHTML = clusterString;
         }
       );
-
       if (e.features.length === 0) return;
-
       document.getElementById("clickedPanel").setAttribute("display", "block");
-
       if (locID) {
         map.removeFeatureState({
           source: "locations",
           id: locID,
         });
       }
-
       locID = e.features[0].id;
-
       map.setFeatureState(
         {
           source: "locations",
@@ -241,42 +321,77 @@ MapBoxMap.prototype.initVis = function () {
 
     map.on("click", "unclustered-point", (e) => {
       var currData = e.features[0].properties;
-      document.getElementById("displayList").innerHTML = "";
+      document.getElementById("clusterList").innerHTML = "";
       document.getElementById("clickedPanel").setAttribute("display", "none");
-      // var displayString = "<strong>Personnel: </strong> <br>";
-      // if (currData.personnel.length > 0) {
-      //   currData.personnel.forEach(function (p) {
-      //     displayString = displayString + "<a>" + p["Full Name"] + "</a> <br>";
-      //   });
-      // }
+      // console.log(currData);
       popup
         .setLngLat(e.lngLat)
         .setHTML(
           "<div id = 'locPopup'><h5>" +
             currData.name +
             ", <br>" +
-            "New York</h5>" +
-            "<strong>Built in: " +
-            currData.firstYear +
-            "</strong></div>"
+            currData.state +
+            ", " +
+            currData.country +
+            "</h5>" +
+            "</div>"
         )
         .addTo(map);
-      // document.getElementById("displayList").innerHTML = displayString;
 
+      if (currData.personnel != undefined) {
+        e.features[0].properties.personnel = JSON.parse(
+          e.features[0].properties.personnel
+        );
+        // console.log("personnel")
+      }
       // perhaps populate a scrollable list if there are personnel?
     });
 
-    map.on("mousemove", "counties", (county) => {
+    map.on("mousemove", "County Fill", (county) => {
       var currData = county.features[0].properties;
-
-      document.getElementById("displayList").innerHTML =
+      document.getElementById("countyInfo").innerHTML =
         "<h5><strong>Hovered County: </strong>" +
-        currData["NAME"] +
+        currData["NAME"].charAt(0) +
+        currData["NAME"].slice(1).toLowerCase() +
         ", " +
         currData["STATE_TERR"] +
         "</h5>";
     });
-    // cursor so that user knows to click
+    map.on("mouseleave", "County Fill", () => {
+      document.getElementById("countyInfo").innerHTML = "";
+    });
+    map.on("mousemove", "State Fill", (state) => {
+      var currData = state.features[0].properties;
+      document.getElementById("stateInfo").innerHTML =
+        "<h5><strong>Hovered State: </strong>" + currData["NAME"] + "</h5>";
+    });
+    map.on("mouseleave", "State Fill", () => {
+      document.getElementById("stateInfo").innerHTML = "";
+    });
+    map.on("mousemove", "1798 Tax Divisions", (tax) => {
+      var currData = tax.features[0].properties;
+      document.getElementById("taxInfo").innerHTML =
+        "<h5><strong>Hovered 1798 Tax Division: </strong>" +
+        currData["TaxDivision1798"] +
+        ", " +
+        currData["State"] +
+        "</h5>";
+    });
+    map.on("mouseleave", "1798 Tax Divisions", () => {
+      document.getElementById("taxInfo").innerHTML = "";
+    });
+    map.on("mousemove", "1814 District", (district) => {
+      var currData = district.features[0].properties;
+      document.getElementById("districtInfo").innerHTML =
+        "<h5><strong>Hovered 1814 District: </strong>" +
+        currData["District1814"] +
+        ", " +
+        currData["State"] +
+        "</h5>";
+    });
+    map.on("mouseleave", "1814 District", () => {
+      document.getElementById("districtInfo").innerHTML = "";
+    });
     map.on("mouseenter", "clusters", () => {
       map.getCanvas().style.cursor = "pointer";
     });
@@ -294,9 +409,6 @@ MapBoxMap.prototype.initVis = function () {
       .getElementById("timeSlider")
       .addEventListener("change", function (e) {
         var sliderYear = e.target.value + "-12-31";
-
-        // county data glitch at 1826, missing spot
-
         var newCountyData = {
           ...vis.data[1], // Spread the existing object properties
           features: vis.data[1].features.filter(function (d) {
@@ -319,20 +431,159 @@ MapBoxMap.prototype.initVis = function () {
             return bool;
           }), // Filter the array based on a condition
         };
+        console.log(document.getElementById("locYearSwitch").value);
+        if (document.getElementById("locYearSwitch").value) {
+          var currYear = document.getElementById("timeSlider").value;
+          var newLocData = {
+            ...vis.data[0],
+            features: vis.data[0].features.filter(function (d) {
+              var bool = false;
+              console.log(d);
 
-        var newLocData = {
-          ...vis.data[0],
-          features: vis.data[0].features.filter(function (d) {
-            return d.properties.firstYear <= e.target.value;
-          }),
-        };
+              // console.log(d, d["START_DATE"])
+              if (
+                !isNaN(d.properties.firstYear) ||
+                !isNaN(d.properties.lastYear)
+              ) {
+                bool =
+                  d.properties.firstYear >= currYear &&
+                  d.properties.lastYear <= currYear;
+              }
+              return bool;
+            }),
+          };
+          map.getSource("locations").setData(newLocData);
+        } else {
+          map.getSource("locations").setData(vis.data[0]);
+        }
+
         map.getSource("countyBoundaries").setData(newCountyData);
-        map.getSource("locations").setData(newLocData);
         map.getSource("stateBoundaries").setData(newStateData);
       });
-    console.log(vis.data);
+    document
+      .getElementById("locYearSwitch")
+      .addEventListener("input", function (switchValue) {
+        console.log(switchValue);
+        if (document.getElementById("locYearSwitch").value) {
+          var currYear = document.getElementById("timeSlider").value;
+          var newLocData = {
+            ...vis.data[0],
+            features: vis.data[0].features.filter(function (d) {
+              var bool = false;
+              console.log(d);
 
+              // console.log(d, d["START_DATE"])
+              if (
+                !isNaN(d.properties.firstYear) ||
+                !isNaN(d.properties.lastYear)
+              ) {
+                bool =
+                  d.properties.firstYear >= currYear &&
+                  d.properties.lastYear <= currYear;
+              }
+              return bool;
+            }),
+          };
+          map.getSource("locations").setData(newLocData);
+          document.getElementById("locYearSwitch").value = false;
+        } else {
+          map.getSource("locations").setData(vis.data[0]);
+          document.getElementById("locYearSwitch").value = true;
+        }
+      });
+
+    console.log(vis.data);
     // CLOSE OF MAPLOAD
+  });
+
+  map.on("idle", () => {
+    // If these two layers were not added to the map, abort
+    if (
+      !map.getLayer("State Boundaries") ||
+      !map.getLayer("County Boundaries") ||
+      !map.getLayer("1814 District") ||
+      !map.getLayer("1798 Tax Divisions") ||
+      !map.getLayer("State Fill") ||
+      !map.getLayer("County Fill")
+    ) {
+      return;
+    }
+
+    // Enumerate ids of the layers.
+    const toggleableLayerIds = [
+      "State Boundaries",
+      "County Boundaries",
+      "1814 District",
+      "1798 Tax Divisions",
+    ];
+
+    // Set up the corresponding toggle button for each layer.
+    for (const id of toggleableLayerIds) {
+      // Skip layers that already have a button set up.
+      if (document.getElementById(id)) {
+        continue;
+      }
+
+      // Create a link.
+      const link = document.createElement("a");
+      link.id = id;
+      link.href = "#";
+      link.textContent = id;
+      link.className = "";
+
+      // Show or hide layer when the toggle is clicked.
+      link.onclick = function (e) {
+        const clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        const visibility = map.getLayoutProperty(clickedLayer, "visibility");
+
+        // Toggle layer visibility by changing the layout object's visibility property.
+        if (visibility === "visible") {
+          map.setLayoutProperty(clickedLayer, "visibility", "none");
+          this.className = "";
+        } else {
+          this.className = "active";
+          map.setLayoutProperty(clickedLayer, "visibility", "visible");
+        }
+        if (clickedLayer == "State Boundaries") {
+          if (map.getLayoutProperty("State Fill", "visibility") === "visible") {
+            map.setLayoutProperty("State Fill", "visibility", "none");
+          } else {
+            map.setLayoutProperty("State Fill", "visibility", "visible");
+          }
+        } else if (clickedLayer == "County Boundaries") {
+          if (
+            map.getLayoutProperty("County Fill", "visibility") === "visible"
+          ) {
+            map.setLayoutProperty("County Fill", "visibility", "none");
+          } else {
+            map.setLayoutProperty("County Fill", "visibility", "visible");
+          }
+        } else if (clickedLayer == "1798 Tax Divisions") {
+          if (
+            map.getLayoutProperty("Tax Outlines", "visibility") === "visible"
+          ) {
+            map.setLayoutProperty("Tax Outlines", "visibility", "none");
+          } else {
+            map.setLayoutProperty("Tax Outlines", "visibility", "visible");
+          }
+        } else if (clickedLayer == "1814 District") {
+          if (
+            map.getLayoutProperty("District Outlines", "visibility") ===
+            "visible"
+          ) {
+            map.setLayoutProperty("District Outlines", "visibility", "none");
+          } else {
+            map.setLayoutProperty("District Outlines", "visibility", "visible");
+          }
+        }
+      };
+
+      const layers = document.getElementById("toggleMenu");
+      layers.appendChild(link);
+    }
   });
 
   // CLOSE OF INITVIS
